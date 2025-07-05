b07d5bb3bba2ed67341036c5bd0a2cc32ddd3110
gourav mukherjee <gourav.mukherjee@7-11.com>
2025-07-05 21:23:12 +0530

    html file update

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-function baseUrlReplacePlugin() {
-  return {
-    name: 'html-transform-base-url',
-    transformIndexHtml(html, ctx) {
-      const base = ctx.server ? '/' : '/gaming-site-project/';
-      return html.replace(/%VITE_BASE_URL%/g, base);
-    },
-  };
-}
-
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: [react(), baseUrlReplacePlugin()],
+  plugins: [react()],
   base: command === 'build' ? '/gaming-site-project/' : '/'
 }))
